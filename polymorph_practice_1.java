8b46b3865a1a7096aa5afcce95b438f54269ee9b
MD Ridoy Babu <147362353+mdrezuanislamridoy@users.noreply.github.com>
2023-11-12 11:30:02 +0600

    question add

--- polymorph_practice_1.java
+++ polymorph_practice_1.java
@@ -1,1 +1,2 @@
 
+//Write a Java program to create a class Vehicle with a method called speedUp(). Create two subclasses Car and Bicycle. Override the speedUp() method in each subclass to increase the vehicle's speed differently.
